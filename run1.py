35759f805887389c7d8fab23a5f5ecbcbe6a8ff8
Khalili <Jonas.Khalili@itelligence.dk>
2020-04-14 14:42:24 +0200

    argparse fix

--- run1.py
+++ run1.py
@@ -1,6 +1,10 @@
-import argparse 
+# import argparse 
 
-parse = argparse.ArgumentParser(description="ForTest!")
-parse.add_argument('integers', type=int)
+# parse = argparse.ArgumentParser(description="ForTest!")
+# parse.add_argument('integers', type=int)
 
-print(parse.parse_args())
+# print(parse.parse_args())
+
+import sys
+
+print("Result:", sys.argv) 
